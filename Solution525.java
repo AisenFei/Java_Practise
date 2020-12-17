34defc8028d6b466a612cc5eca75e422508b858c
AisenFei <1374873433@qq.com>
2020-12-17 21:59:02 +0800

    连续数组

---
+++ Solution525.java
@@ -0,0 +1,21 @@
+package code_2020_1215;
+//525题，连续数组
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution525 {
+    public int findMaxLength(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(0,-1);
+        int maxLen = 0,count = 0;
+        for(int i = 0;i < nums.length;i++){
+            count = count + (nums[i] == 1 ? 1 : -1);
+            if(map.containsKey(count)){
+                maxLen = Math.max(maxLen,i - map.get(count));
+            }else{
+                map.put(count,i);
+            }
+        }
+        return maxLen;
+    }
+}
