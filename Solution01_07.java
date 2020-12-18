fcf7a96206173dd703bd87fcea8597931a2ddeaa
AisenFei <1374873433@qq.com>
2020-12-18 21:38:48 +0800

    旋转数组

---
+++ Solution01_07.java
@@ -0,0 +1,23 @@
+package code_2020_1215;
+//面试题01.07旋转数组
+class Solution01_07 {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        // 水平翻转
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = temp;
+            }
+        }
+        // 主对角线翻转
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+}
