0af95d426e2d2ec9bdb3f5f28eb96fb582c08bf3
AisenFei <1374873433@qq.com>
2021-02-11 22:56:20 +0800

    日常练习

---
+++ KthLargest.java
@@ -0,0 +1,24 @@
+package code_2021_0203;
+
+import java.util.PriorityQueue;
+
+public class KthLargest {
+    PriorityQueue<Integer> pq;
+    int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        pq = new PriorityQueue<Integer>();
+        for (int x : nums) {
+            add(x);
+        }
+    }
+
+    public int add(int val) {
+        pq.offer(val);
+        if (pq.size() > k) {
+            pq.poll();
+        }
+        return pq.peek();
+    }
+}
