c87a9cab9d2b5ecedffc89101999f4bc8124a940
AisenFei <1374873433@qq.com>
2021-02-13 21:53:23 +0800

    日常练习

---
+++ Solution2021_0213.java
@@ -0,0 +1,21 @@
+package code_2021_0203;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution2021_0213 {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int n = nums.length;
+        for (int num : nums) {
+            int x = (num - 1) % n;
+            nums[x] += n;
+        }
+        List<Integer> ret = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= n) {
+                ret.add(i + 1);
+            }
+        }
+        return ret;
+    }
+}
