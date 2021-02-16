6a1ad287bb4ed30dbc386f6ed44a6c0f425d9fc9
AisenFei <1374873433@qq.com>
2021-02-16 23:51:31 +0800

    日常练习

---
+++ Solution2021_0216.java
@@ -0,0 +1,14 @@
+package code_2021_0203;
+
+import java.util.Arrays;
+
+public class Solution2021_0216 {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int ans = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            ans += nums[i];
+        }
+        return ans;
+    }
+}
