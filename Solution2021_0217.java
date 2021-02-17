30d7d6731dc890dec789de8fa9462b0d232f911b
AisenFei <1374873433@qq.com>
2021-02-17 21:56:53 +0800

    日常练习

---
+++ Solution2021_0217.java
@@ -0,0 +1,17 @@
+package code_2021_0203;
+
+public class Solution2021_0217 {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        int m = nums.length;
+        int n = nums[0].length;
+        if (m * n != r * c) {
+            return nums;
+        }
+
+        int[][] ans = new int[r][c];
+        for (int x = 0; x < m * n; ++x) {
+            ans[x / c][x % c] = nums[x / n][x % n];
+        }
+        return ans;
+    }
+}
