7d46e6e039b8ca1dde41d44af597e3f4ffc536b2
AisenFei <1374873433@qq.com>
2021-02-21 21:22:58 +0800

    日常练习

---
+++ Solution2021_0221.java
@@ -0,0 +1,24 @@
+package code_2021_0203;
+
+import java.util.TreeMap;
+public class Solution2021_0221 {
+    public int longestSubarray(int[] nums, int limit) {
+        TreeMap<Integer, Integer> map = new TreeMap<Integer, Integer>();
+        int n = nums.length;
+        int left = 0, right = 0;
+        int ret = 0;
+        while (right < n) {
+            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+            while (map.lastKey() - map.firstKey() > limit) {
+                map.put(nums[left], map.get(nums[left]) - 1);
+                if (map.get(nums[left]) == 0) {
+                    map.remove(nums[left]);
+                }
+                left++;
+            }
+            ret = Math.max(ret, right - left + 1);
+            right++;
+        }
+        return ret;
+    }
+}
