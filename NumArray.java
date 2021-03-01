dca80d88d75606b3eeae3ce1b35674a63d4f2d9e
AisenFei <1374873433@qq.com>
2021-03-01 22:46:01 +0800

    日常练习

---
+++ NumArray.java
@@ -0,0 +1,17 @@
+package code2021_0301;
+
+public class NumArray {
+    int[] sums;
+
+    public NumArray(int[] nums) {
+        int n = nums.length;
+        sums = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            sums[i + 1] = sums[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sums[j + 1] - sums[i];
+    }
+}
