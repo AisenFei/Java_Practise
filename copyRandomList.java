3c7905d5a6538da262bf43d5d7ad6f46000c2e3d
AisenFei <63179295+AisenFei@users.noreply.github.com>
2021-04-04 23:50:43 +0800

    日常练习

---
+++ copyRandomList.java
@@ -0,0 +1,9 @@
+package ListNode;
+
+//35. 复杂链表的复制
+public class copyRandomList {
+    public ListNode solution (ListNode head) {
+        ListNode prev = new ListNode(-1);
+        return prev.next;
+    }
+}
